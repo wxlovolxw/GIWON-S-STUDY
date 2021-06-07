869110da791de92f486c66a9f64603a1268cbceb
wxlovolxw <62921934+wxlovolxw@users.noreply.github.com>
2021-06-08 03:55:25 +0900

    6/8 Django polls(3) - 관리자사이트

--- Django/project1_polls/mysite/polls/admin.py
+++ Django/project1_polls/mysite/polls/admin.py
@@ -1,3 +1,8 @@
-from django.contrib import admin
+# polls/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import Question
+
+admin.site.register(Question)
+
+
